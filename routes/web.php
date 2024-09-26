02bf0703ee40acc29a709067c044156ff5d07855
Md. Hafizur Rahman <hafiz-rahman@live.com>
2024-09-27 02:27:53 +0600

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('home');
+    });
+
+    Route::get('/about', function () {
+        return view('about');
+    });
+
+    Route::get('/contact', function () {
+        return view('contact');
+    });
