8ea4a4f21c21b5c6c1942d5df916675e82d4dfbd
Hafizur Rahman <hafiz-rahman@live.com>
2024-10-14 03:19:18 +0600

    setup tags

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,14 +2,24 @@
 
     namespace App\Models;
 
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
-    class Job extends Model
-    {
+    class Job extends Model {
         use HasFactory;
 
         protected $table = 'job_listings';
+
         protected $fillable = ['title', 'salary'];
+
+        public function employer()
+        {
+            return $this->belongsTo(Employer::class);
+        }
+
+        public function tags()
+        {
+            return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+
+        }
     }
