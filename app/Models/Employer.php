bc67a71ab72fe5ec03c4e3d1628333d462cedd5f
Hafizur Rahman <hafiz-rahman@live.com>
2024-10-19 02:44:19 +0600

    database seeder

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App\Models;
+    namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
 
-class Employer extends Model
-{
-    use HasFactory;
-
-    public function jobs()
+    class Employer extends Model
     {
-        return $this->hasMany(Job::class);
+        use HasFactory;
+
+        public function jobs()
+        {
+            return $this->hasMany(Job::class);
+        }
     }
-}
